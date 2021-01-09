3a2ab53ccb68d4154757dd7c49a1f55bd2818c7d
Ruhani-Singh <77180797+Ruhani-Singh@users.noreply.github.com>
2021-01-08 18:17:05 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// Change image on on Mouse Over. 
+function changeimage() {
+  document.getElementById("image1").src = "https://images.unsplash.com/photo-1605189101952-f1efac73fc8c?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ";
+}
+function flipimage2() {
+  document.getElementById("image2").src = "https://images.unsplash.com/photo-1470265164177-33def6c4a87e?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=85";
+}
